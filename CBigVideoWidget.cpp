cc1d1a3ffb71409b0941b0c4c334fd29cf32b526
wuyansai <2539221675@qq.com>
2025-03-13 18:46:17 +0800

    Initial commit

---
+++ CBigVideoWidget.cpp
@@ -0,0 +1,23 @@
+#include "CBigVideoWidget.h"
+
+
+CBigVideoWidget::CBigVideoWidget(QWidget* p)
+	:QWidget(p)
+{
+	this->setMinimumSize(800, 600);
+
+	setAttribute(Qt::WA_StyledBackground);
+	setAttribute(Qt::WA_OpaquePaintEvent);
+
+	setStyleSheet("background-color:rgb(210,220,230)");
+}
+
+CBigVideoWidget::~CBigVideoWidget()
+{
+
+}
+
+HWND CBigVideoWidget::getHwnd() const
+{
+	return (HWND)(this->winId());
+}
